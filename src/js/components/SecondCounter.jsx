38cef49cb1408b155d77022fd8f3b12920571cf2
MaTi <matiasnicolaaas@gmail.com>
2025-01-21 00:30:16 +0000

    verificacion 1

---
+++ src/js/components/SecondCounter.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SecondCounter = (props) => {
+    return (
+     <>
+        
+         <h2> {props.seconds}</h2>
+
+    </>
+    ) 
+}
+
+export default SecondCounter 
+ 
